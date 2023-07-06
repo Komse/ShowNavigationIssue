24a7fcbe6c3875e7897466b85f3b59c066575307
Komse <>
2023-07-06 15:49:37 +0200

    Feat: Backward button inside yellow view

--- ShowNavigationIssue/YellowView.swift
+++ ShowNavigationIssue/YellowView.swift
@@ -1,9 +1,22 @@
 import SwiftUI
 
 struct YellowView: View {
+    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
+    
     var body: some View {
         ZStack {
             Color.yellow.edgesIgnoringSafeArea(.all)
+            Button(
+                action: { presentationMode.wrappedValue.dismiss() },
+                label: {
+                    HStack{
+                        Image(systemName: "chevron.backward")
+                        Text("Back")
+                    }
+                }
+            )
+            .font(.system(size: 17))
         }
+        .navigationBarBackButtonHidden(true)
     }
 }
