dda2618906f31fde639f0fcff0f76d9596093e3d
Komse <>
2023-07-06 15:55:29 +0200

    Feat: Remove hello world

--- ShowNavigationIssue/ContentView.swift
+++ ShowNavigationIssue/ContentView.swift
@@ -6,10 +6,6 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
                 goToYellowButton()
                 goToYellowScreenCoverButton()
             }
